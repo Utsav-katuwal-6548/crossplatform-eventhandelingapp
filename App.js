7d7365be5a10ccc63dbb34bf6b6f40c94eca84c7
utsavkatuwal <6548@ait.nsw.edu.au>
2020-08-15 16:08:54 +1000

    addingbutton

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from 'react';
+import {  StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+import InputBar from'./components/InputBar';
+
+export default class  App extends React.Component {
+  constructor(){
+       super();
+       this.state ={
+         todoInput: '',
+         todos: [
+           {id: 0, title: 'take the trash out', done:false},
+           {id:0, title: 'cook dinner', done:false}
+         ]
+       }
+
+  }
+
+
+
+
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.StatusBar}>
+        
+      </View>
+      <Header  title="TODOAPP"/>
+      <InputBar/>
+      
+
+      
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  StatusBar:{
+    backgroundColor:'#FFCE00',
+    height:20
+  }
 });
